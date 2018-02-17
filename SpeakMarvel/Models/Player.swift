1b88cd2916c8616eac0c162cb84eefba44df5d55
Haejin Jo <professionalhaejin@gmail.com>
2018-02-16 23:57:12 -0800

    Input button images, started MVC files

---
+++ SpeakMarvel/Models/Player.swift
@@ -0,0 +1,9 @@
+//
+//  Player.swift
+//  SpeakMarvel
+//
+//  Created by Admin on 2/16/18.
+//  Copyright © 2018 meera. All rights reserved.
+//
+
+import Foundation
---
+++ SpeakMarvel/Models/Quote.swift
@@ -0,0 +1,9 @@
+//
+//  Quote.swift
+//  SpeakMarvel
+//
+//  Created by Admin on 2/16/18.
+//  Copyright © 2018 meera. All rights reserved.
+//
+
+import Foundation
