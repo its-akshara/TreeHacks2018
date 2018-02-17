ba590ef0dfa79a84dea6b1531974fbb8a3746afb
Sanchit Sinha <sinhasunny1979@gmail.com>
2018-02-17 00:44:25 -0800

    Setup gcloud

--- Marvelous-backend/app.js
+++ Marvelous-backend/app.js
@@ -1,46 +1,33 @@
-port = 8080
-site = 'localhost'
-
-StaticServer = require('./static-server');
-app = require('express')();
-path = require("path");
-
-staticServer = new StaticServer();
-
-app.use('/data', staticServer);
-
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
-});
-
-port = 8080
-
-app.listen(port, function () {
-    console.log('Marvelous hosted at http://localhost:' + port);
-});
-
-// Cloud import
-// Imports the Google Cloud client library
-const Storage = require('@google-cloud/storage');
-
-// Creates a client
-const storage = new Storage();
-
 /**
- * TODO(developer): Uncomment the following lines before running the sample.
+ * Copyright 2017, Google, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-// const bucketName = 'Name of a bucket, e.g. my-bucket';
-// const filename = 'Local file to upload, e.g. ./local/path/to/file.txt';
 
-// Uploads a local file to the bucket
-storage
-  .bucket(bucketName)
-  .upload(filename)
-  .then(() => {
-    console.log(`${filename} uploaded to ${bucketName}.`);
-  })
-  .catch(err => {
-    console.error('ERROR:', err);
-  });
+'use strict';
 
+// [START app]
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, world!').end();
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+// [END app]
